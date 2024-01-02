195d1377ab35b355ee1cb4628879b5d76746efa3
MasonGarrett <thegarretts99@gmail.com>
2024-01-02 08:48:39 -0500

    Add image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import { getServerSession } from 'next-auth';
-import Link from 'next/link';
+import Image from 'next/image';
 import { authOptions } from './api/auth/[...nextauth]/route';
-import ProductCard from './components/ProductCard';
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
 
     return (
-        <main>
-            <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-            <Link href="/users">Users</Link>
-            <ProductCard />
+        <main className="relative h-screen">
+            <Image
+                src="https://bit.ly/react-cover"
+                alt="coffee"
+                fill
+                className="object-cover"
+                sizes="(max-width: 480px) 100vw, (max-widh: 768px) 50vw, 33vw"
+                quality={100}
+                priority
+            />
         </main>
     );
 }
